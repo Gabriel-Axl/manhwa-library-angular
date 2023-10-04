86ef12752e272721f34d3a6ddc43468ed857c222
alexgab <alexgabriel3357@gmail.com>
2023-10-04 01:32:24 -0300

    responsividade do menu

--- library-app/src/app/components/main-menu/main-menu.component.ts
+++ library-app/src/app/components/main-menu/main-menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faList } from '@fortawesome/free-solid-svg-icons';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-main-menu',
@@ -9,10 +11,12 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 export class MainMenuComponent implements OnInit {
 
   faSearch = faSearch;
-  
+  faList = faList;
+  faClock = faClock;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
 }
