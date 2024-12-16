e8e57bdc5843c8fa4e80d41d66f083c7fce7bd6b
alexgab <alexgabriel3357@gmail.com>
2024-12-16 14:22:04 -0300

    adicionando servidor para deploy

--- library-app/src/environments/environment.prod.ts
+++ library-app/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDwV-ioHy-3yhpvd1TFz3zV7dJSlewXw70",
+    authDomain: "sololibrary-f6975.firebaseapp.com",
+    projectId: "sololibrary-f6975",
+    storageBucket: "sololibrary-f6975.appspot.com",
+    messagingSenderId: "327724242318",
+    appId: "1:327724242318:web:3e64ba70379f65c42e40df",
+    measurementId: "G-7Y65DBTX4T"
+  }
 };
